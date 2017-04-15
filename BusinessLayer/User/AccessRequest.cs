a6a2fbfec58f11634ad942da2259d61522c02d2f
mhsjaber <mhsjaber@JABER>
2017-04-15 16:46:24 +0600

    modified

--- BusinessLayer/User/AccessRequest.cs
+++ BusinessLayer/User/AccessRequest.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DataAccessLayer.User
 {
     public class AccessRequest
     {
+        [Required]
         public Guid ID { get; set; }
+        [Required]
         public AccessType Type { get; set; }
+        [Required]
         public Guid RequestByID { get; set; }
+        [Required]
         public Guid RequestToID { get; set; }
+        [Required]
         public AccessRequestStatus Status { get; set; }
+        [Required]
         public DateTime CreatedOn { get; set; }
+        [Required]
         public DateTime ApprovedOn { get; set; }
     }
 }
--- BusinessLayer/User/Block.cs
+++ BusinessLayer/User/Block.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DataAccessLayer.User
 {
     public class Block
     {
+        [Required]
         public Guid ID { get; set; }
+        [Required]
         public Guid UserID { get; set; }
+        [Required]
         public Guid BlockID { get; set; }
+        [Required]
         public DateTime CreatedOn { get; set; }
     }
 }
